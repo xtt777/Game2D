c1121371d5bd784668463009f33f0bfb259518f2
xtt123 <xxtng28@gmail.com>
2024-11-18 17:09:30 +0800

    2

--- app/src/main/java/com/example/game2d/Game.kt
+++ app/src/main/java/com/example/game2d/Game.kt
@@ -1,4 +1,24 @@
-package com.example.game2d
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 
-class Game {
+class Game(val scope: CoroutineScope, val screenW:Int, val screenH: Int) {
+    var counter = 0
+    val state = MutableStateFlow(0)
+    val background = Background(screenW)
+    var isPlaying = true
+
+    fun Play(){
+        scope.launch {
+            isPlaying = true
+
+            while (isPlaying) {
+                delay(1000)
+                background.Roll()
+                counter++
+                state.emit(counter)
+            }
+        }
+    }
 }
